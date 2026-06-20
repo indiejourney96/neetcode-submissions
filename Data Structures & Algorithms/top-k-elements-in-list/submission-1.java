8636e23d131d57de1be81aa5c4bc44dbcfb06b1c
indiejourney96 <113288805+indiejourney96@users.noreply.github.com>
2026-06-20 13:34:39 +0800

    Update: top-k-elements-in-list - submission-1

--- Data Structures & Algorithms/top-k-elements-in-list/submission-1.java
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.java
@@ -1,56 +1,47 @@
-public class Solution {
+class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-        // Step 1: Count frequency of each number
+        // HashMap<Number, Frequency>
         // Example:
         // nums = [1,1,1,2,2,3]
         // count = {1=3, 2=2, 3=1}
         Map<Integer, Integer> count = new HashMap<>();
 
-        // Step 2: Create "buckets"
-        // freq[i] = list of numbers that appear exactly i times
-        // Index = frequency, Value = list of numbers
-        //
-        // Example after building:
-        // freq[3] = [1]
-        // freq[2] = [2]
-        // freq[1] = [3]
-        List<Integer>[] freq = new List[nums.length + 1];
-
-        // Initialize each bucket list
-        for (int i = 0; i < freq.length; i++) {
-            freq[i] = new ArrayList<>();
-        }
-
-        // Build frequency map
-        for (int n : nums) {
-            count.put(n, count.getOrDefault(n, 0) + 1);
-        }
-
-        // Place numbers into their frequency bucket
-        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
-            int number = entry.getKey();
-            int frequency = entry.getValue();
-
-            freq[frequency].add(number);
-        }
-
-        // Step 3: Collect results starting from highest frequency
-        int[] res = new int[k];
-        int index = 0;
-
-        // Go from high frequency -> low frequency
-        for (int i = freq.length - 1; i > 0 && index < k; i--) {
-
-            // Add all numbers in this frequency bucket
-            for (int n : freq[i]) {
-                res[index++] = n;
-
-                // Stop early once we have k elements
-                if (index == k) {
-                    return res;
-                }
+        // Count frequency of each number
+        for (int num : nums) {
+            if (count.containsKey(num)) {
+                count.put(num, count.get(num) + 1);
+            } else {
+                count.put(num, 1);
             }
+        }
+
+        // Convert HashMap into a sortable list
+        // Store as [frequency, number]
+        // Example:
+        // {1=3, 2=2, 3=1}
+        // ->
+        // [[3,1], [2,2], [1,3]]
+        List<int[]> arr = new ArrayList<>();
+
+        for (int key : count.keySet()) {
+            arr.add(new int[] {count.get(key), key});
+        }
+
+        // Sort by frequency descending (highest frequency first)
+        // b[0] = frequency of b
+        // a[0] = frequency of a
+        // Example after sort:
+        // [[3,1], [2,2], [1,3]]
+        arr.sort((a, b) -> Integer.compare(b[0], a[0]));
+
+        int[] res = new int[k];
+
+        // Take the first k numbers after sorting
+        // arr.get(i)[1] = actual number
+        // arr.get(i)[0] = frequency
+        for (int i = 0; i < k; i++) {
+            res[i] = arr.get(i)[1];
         }
 
         return res;
