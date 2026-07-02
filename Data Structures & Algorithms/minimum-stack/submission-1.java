5b71af238f8d949bf3724ad8b182af291ac0d08f
indiejourney96 <113288805+indiejourney96@users.noreply.github.com>
2026-07-02 15:36:48 +0800

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,34 @@
+class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minTemp;
+    public MinStack() {
+        stack = new Stack<>();
+        minTemp = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minTemp.isEmpty() || val <= minTemp.peek()){
+            minTemp.push(val);
+        }
+    }
+    
+    public void pop() {
+        if (stack.isEmpty()) {
+            return;
+        }
+        int top = stack.pop();
+        if (minTemp.peek() == top){
+            minTemp.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minTemp.peek();
+    }
+}
