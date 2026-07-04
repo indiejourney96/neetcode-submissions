dfe5511b435da278e9933778eb20b2977b00ebae
indiejourney96 <113288805+indiejourney96@users.noreply.github.com>
2026-07-04 13:16:03 +0800

    Update: anagram-groups - submission-0

--- Data Structures & Algorithms/anagram-groups/submission-0.java
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -1,13 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> res = new HashMap<>(); //Key, Value : "aet"->["eat","tea","ate"]
+        Map<String, List<String>> ans = new HashMap<>();
+        //Key, Value : "aet"->["eat","tea","ate"]
         for (String s : strs){
             char[] charArray = s.toCharArray(); //['e','a','t']
             Arrays.sort(charArray); //['a','e','t']
-            String sortedStrs = new String(charArray);
-            res.putIfAbsent(sortedStrs, new ArrayList<>()); // "aet" : [] 
-            res.get(sortedStrs).add(s); //   "aet" : ["eat"]
+            String sortedStr = new String (charArray);
+            ans.putIfAbsent(sortedStr, new ArrayList<>()); // "aet" : [] 
+            ans.get(sortedStr).add(s);//   "aet" : ["eat"]
         }
-        return new ArrayList<>(res.values());
+        List<List<String>> res = new ArrayList<>(ans.values()); 
+        return res;
     }
 }
