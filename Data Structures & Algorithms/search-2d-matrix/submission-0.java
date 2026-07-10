745d142c93673a854b1ef5d1886e46b9edf82135
indiejourney96 <113288805+indiejourney96@users.noreply.github.com>
2026-07-10 10:53:24 +0800

    Update: search-2d-matrix - submission-0

--- Data Structures & Algorithms/search-2d-matrix/submission-0.java
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.java
@@ -10,3 +10,9 @@ class Solution {
         return false;
     }
 }
+
+
+//Because each row is sorted and the first element of each row is greater than the last element of the previous row, 
+//the matrix can be treated as one sorted array. 
+//I perform binary search on indices from 0 to ROWS * COLS - 1. 
+//For each midpoint m, I map it back to row = m / COLS and col = m % COLS
