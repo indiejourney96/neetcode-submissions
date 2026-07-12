89ba60fdc0243e839f80cb122cf002e5a79a2f90
indiejourney96 <113288805+indiejourney96@users.noreply.github.com>
2026-07-12 13:22:01 +0800

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.java
@@ -0,0 +1,36 @@
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int ROWS = matrix.length, COLS = matrix[0].length;
+
+        int top = 0, bot = ROWS - 1;
+        while (top <= bot){ //find for the row which target belongs to
+            int row = (top + bot) / 2; 
+            if (matrix[row][COLS - 1] < target){
+                top = row + 1;
+            } else if (matrix[row][0] > target){
+                bot = row - 1;
+            } else {
+                break;
+            }
+        }
+
+        if (!(top <= bot)){  // No valid row found
+            return false;
+        }
+
+        int row = (top + bot) / 2; //declare again because the previous row was inside the loop. after the loop, it is no longer available.
+        int l = 0, r = COLS - 1; 
+        while (l <= r){ //find the specific column with the determined row.
+            int m = (l + r) / 2 ; 
+            if (matrix[row][m] < target){
+                l = m + 1; 
+            } else if (matrix[row][m] > target){
+                r = m - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
