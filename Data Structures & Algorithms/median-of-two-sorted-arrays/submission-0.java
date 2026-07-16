c59b4f2edfaed2411e8833fda65a5b7776df60f8
indiejourney96 <113288805+indiejourney96@users.noreply.github.com>
2026-07-16 14:23:32 +0800

    Add: median-of-two-sorted-arrays - submission-0

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] merged = new int[nums1.length + nums2.length];
+
+        int index = 0; 
+        double ans = 0;
+
+        //Copy num1 
+        for (int num : nums1){
+            merged[index++] = num;
+        }
+
+        //Copy num2
+        for (int num : nums2){
+            merged[index++] = num;
+        }
+
+        Arrays.sort(merged);
+
+        int n = merged.length;
+
+        //Odd length 
+        if (n % 2 == 1){
+            ans = merged[n / 2];
+        } 
+        else if (n % 2 == 0) { //Even length
+            ans = (merged[n / 2 - 1] + merged[n / 2]) / 2.0;
+        }
+        
+        return ans;
+    }
+}
