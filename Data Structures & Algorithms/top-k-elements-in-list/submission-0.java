aa596345ba4627e416c3751c9bdbdf54b2bb1309
indiejourney96 <113288805+indiejourney96@users.noreply.github.com>
2026-07-27 20:19:23 +0800

    Update: top-k-elements-in-list - submission-0

--- Data Structures & Algorithms/top-k-elements-in-list/submission-0.java
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.java
@@ -1,49 +1,35 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> count = new HashMap<>(); // HashMap<Number, Frequency>
 
-        // HashMap<Number, Frequency>
-        // Example:
-        // nums = [1,1,1,2,2,3]
-        // count = {1=3, 2=2, 3=1}
-        Map<Integer, Integer> count = new HashMap<>();
-
-        // Count frequency of each number
-        for (int num : nums) {
-            if (count.containsKey(num)) {
-                count.put(num, count.get(num) + 1);
+        for (int num : nums){ // Count frequency of each number
+            if(count.containsKey(num)){
+                count.put(num , count.get(num) + 1);
             } else {
                 count.put(num, 1);
             }
         }
+        List<int[]> arr = new ArrayList<>(); //Store the HashMap into a sortable list
 
-        // Convert HashMap into a sortable list
-        // Store as [frequency, number]
-        // Example:
-        // {1=3, 2=2, 3=1}
-        // ->
-        // [[3,1], [2,2], [1,3]]
-        List<int[]> arr = new ArrayList<>();
-
-        for (int key : count.keySet()) {
-            arr.add(new int[] {count.get(key), key});
+        for (int key : count.keySet()){
+            arr.add(new int[] {count.get(key), key}); // Store as [frequency, actual number]
         }
 
         // Sort by frequency descending (highest frequency first)
         // b[0] = frequency of b
         // a[0] = frequency of a
-        // Example after sort:
-        // [[3,1], [2,2], [1,3]]
-        arr.sort((a, b) -> Integer.compare(b[0], a[0]));
+        arr.sort((a,b) -> Integer.compare(b[0], a[0]));
 
-        int[] res = new int[k];
+        int[] res = new int[k]; 
 
-        // Take the first k numbers after sorting
-        // arr.get(i)[1] = actual number
+        for ( int i = 0 ; i < k ; i++){
+            res[i] = (arr.get(i)[1]);   
+        // arr.get(i)[1] = actual number      
         // arr.get(i)[0] = frequency
-        for (int i = 0; i < k; i++) {
-            res[i] = arr.get(i)[1];
         }
-
         return res;
     }
 }
+
+//Time complexity: O(n log n)
+//Space complexity: O(n)
