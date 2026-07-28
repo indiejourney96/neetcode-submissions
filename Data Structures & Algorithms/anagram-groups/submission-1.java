e2616636eb155715e68e02bf06de4a15ff8d847f
indiejourney96 <113288805+indiejourney96@users.noreply.github.com>
2026-07-28 20:41:00 +0800

    Update: anagram-groups - submission-1

--- Data Structures & Algorithms/anagram-groups/submission-1.java
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -1,19 +1,25 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> ans = new HashMap<>();
+        HashMap<String, List<String>> res = new HashMap<>();
         //Key, Value : "[1, 0, 0, 0, 1, 0, 0, 0, 0, ..., 1, 0]"->["eat","tea","ate"]
-
+        
         for (String s : strs){
             int[] count = new int[26];
             char[] charArray = s.toCharArray(); //['e','a','t']
             for (char c : charArray){
                 count[c - 'a']++;
             }
-            String sortedStr = Arrays.toString(count); // "[1, 0, 0, 0, 1, 0, 0, 0, 0, ..., 1, 0]"
-            ans.putIfAbsent(sortedStr, new ArrayList<>()); // "[1, 0, 0, 0, 1, 0, 0, 0, 0, ..., 1, 0]" : [] 
-            ans.get(sortedStr).add(s); //"[1, 0, 0, 0, 1, 0, 0, 0, 0, ..., 1, 0]" : ["str"] 
+            String str = Arrays.toString(count);
+            res.putIfAbsent(str, new ArrayList<>()); 
+            // "[1, 0, 0, 0, 1, 0, 0, 0, 0, ..., 1, 0]" : [] 
+            res.get(str).add(s); 
+            //"[1, 0, 0, 0, 1, 0, 0, 0, 0, ..., 1, 0]" : ["str"] 
         }
-        List<List<String>> res = new ArrayList<>(ans.values()); 
-        return res;
+
+        List<List<String>> ans = new ArrayList<>(res.values());
+        return ans;
     }
 }
+
+//Time Complexity: O(n)
+//Space Complexity: O(n)
