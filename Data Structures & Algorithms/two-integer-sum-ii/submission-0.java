820d792cf1134e8538d135b1e64d9844e0ea7a60
indiejourney96 <113288805+indiejourney96@users.noreply.github.com>
2026-08-02 09:59:46 +0800

    Update: two-integer-sum-ii - submission-0

--- Data Structures & Algorithms/two-integer-sum-ii/submission-0.java
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.java
@@ -1,27 +1,24 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int l = 0; int r = numbers.length - 1;
+        int l = 0, r = numbers.length - 1; 
         int[] ans = new int[2];
 
-        if (numbers.length == 2){
-            ans[0] = 1;
-            ans[1] = 2;
-        }
-        
-        
-        while (l < r){
-            if ((numbers[l] + numbers[r]) > target){
+        while ( l < r){
+            if (numbers[l] + numbers[r] > target){
                 r--;
             }
-            if ((numbers[l] + numbers[r]) < target){
+            if (numbers[l] + numbers[r] < target){
                 l++;
             }
             if (numbers[l] + numbers[r] == target){
-            ans[0] = l+1;
-            ans[1] = r+1;
-            break;
+                ans[0] = l + 1;
+                ans[1] = r + 1;
+                break;
             }
         }
         return ans;
     }
 }
+
+//Time Complexity: O(n)
+//Space Complexity: O(1)
