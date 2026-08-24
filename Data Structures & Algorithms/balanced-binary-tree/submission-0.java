ff52be3d232acda3ac6fd5d928d0b872e9204e40
indiejourney96 <113288805+indiejourney96@users.noreply.github.com>
2026-08-24 16:01:03 +0800

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        if (Math.abs(left - right) > 1){
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int getHeight(TreeNode root){
+        if (root == null){
+            return 0;
+        }
+
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+
+        return 1 + Math.max(left,right);
+    }
+}
+
+//Brute Force 
+//Time Complexity: O(n2)
+//Space Complexity: O(n)
+
+//For every node, compute the height of its left subtree.
+//Compute the height of its right subtree.
+//Check if their difference is ≤ 1.
+//Recursively repeat this check for all nodes.
