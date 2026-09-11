4c9e1b2865077765e26b4ff44d2f645b49aa57e4
indiejourney96 <113288805+indiejourney96@users.noreply.github.com>
2026-09-11 09:32:22 +0800

    Update: find-median-in-a-data-stream - submission-0

--- Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.java
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.java
@@ -12,10 +12,15 @@ class MedianFinder {
     public double findMedian() {
         Collections.sort(data);
         int n = data.size();
-        if ((n % 2) == 1){
+        if ((n % 2) == 1){ //odd number
             return data.get(n / 2);
-        } else {
+        } else { //even number
             return (data.get(n / 2) + data.get(n / 2 - 1)) / 2.0;
         }
     }
 }
+
+//Sorting 
+// addNum(num):     O(1)
+// findMedian():    O(N log N)  — sorts every time
+// Space:           O(N)
