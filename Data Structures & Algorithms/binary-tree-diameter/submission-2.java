e172a3a59ae3ed21f7bb4e9d8730c285a47910da
indiejourney96 <113288805+indiejourney96@users.noreply.github.com>
2026-09-25 15:01:26 +0800

    Update: binary-tree-diameter - submission-2

--- Data Structures & Algorithms/binary-tree-diameter/submission-2.java
+++ Data Structures & Algorithms/binary-tree-diameter/submission-2.java
@@ -15,57 +15,34 @@
  */
 
 class Solution {
-
     public int diameterOfBinaryTree(TreeNode root) {
-
-        // Empty tree has diameter 0
         if (root == null) {
             return 0;
         }
 
-        // Find the height of the left subtree
-        int leftHeight = getHeight(root.left);
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
 
-        // Find the height of the right subtree
-        int rightHeight = getHeight(root.right);
+        int diameter = left + right;
 
-        // Diameter passing through the current node
-        int diameter = leftHeight + rightHeight;
-
-        // Find the best diameter in the left subtree
         int leftDiameter = diameterOfBinaryTree(root.left);
 
-        // Find the best diameter in the right subtree
         int rightDiameter = diameterOfBinaryTree(root.right);
 
-        // Return the largest diameter found
-        return Math.max(diameter, Math.max(leftDiameter, rightDiameter));
+
+        return Math.max(diameter,Math.max(leftDiameter,rightDiameter));
     }
 
-
-    // Returns the height of a tree
-    private int getHeight(TreeNode root) {
-
-        // Empty tree has height 0
-        if (root == null) {
-            return 0;
+    public int getHeight(TreeNode root){
+        if (root == null){
+            return 0; 
         }
 
-        // Find height of left subtree
-        int leftHeight = getHeight(root.left);
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
 
-        // Find height of right subtree
-        int rightHeight = getHeight(root.right);
+        int count = 1 + Math.max(left, right);
 
-        // Return height of current node
-        return 1 + Math.max(leftHeight, rightHeight);
+        return count; 
     }
 }
-
-
-//Brute Force 
-//Time Complexity = O(n2)
-//Space complexity = 0(n)
-
-//Brute force: Calculate the height at every node, then recursively calculate the diameter of every subtree.
-//Optimization: While calculating height with DFS, calculate the diameter at the same time so each node is visited only once.
