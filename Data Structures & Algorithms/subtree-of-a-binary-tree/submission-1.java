2f8df06b03e0b75ca8e1ec4e2d39b65609b2d2d5
indiejourney96 <113288805+indiejourney96@users.noreply.github.com>
2026-09-26 09:55:19 +0800

    Update: subtree-of-a-binary-tree - submission-1

--- Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.java
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.java
@@ -17,29 +17,40 @@
 class Solution {  
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if (subRoot == null){
-            return true;
+            return true; //empty tree is considered a subtree
         }
 
         if (root == null){
-            return false;
+            return false; //main tree is empty but subRoot is not, subRoot not found
         }
 
-        if (isSame(root, subRoot)){
-            return true;
+        if (sameTree(root, subRoot)){
+            return true; //check trees starting at the nodes if they are the same
         }
 
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 
-    public boolean isSame(TreeNode p, TreeNode q){
-        if (p == null && q == null){
-            return true;
+    public boolean sameTree(TreeNode root, TreeNode subRoot){
+        if (root == null && subRoot == null){
+            return true; // Both trees reached the end at the same time
         }
 
-        if (p != null && q != null && p.val == q.val){
-            return isSame(p.left, q.left) && isSame(p.right, q.right); 
+        // Both nodes exist and have the same value
+        if (root != null && subRoot != null && root.val == subRoot.val){
+            return sameTree(root.left, subRoot.left) && sameTree(root.right, subRoot.right);
         }
 
-        return false;
+        return false;  // One node is null, or their values are different
     }
 }
+
+
+//Depth First Search (DFS)
+
+//Time Complexity: 0(m * n)
+//Space Complexity: O(m + n)
+
+//m = number of nodes in subRoot 
+//n = number of nodes in root
+
