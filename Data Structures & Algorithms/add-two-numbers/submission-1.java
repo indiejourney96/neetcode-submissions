ec2dab6e4f8da08a5147bbbe50ac67c95138d16e
indiejourney96 <113288805+indiejourney96@users.noreply.github.com>
2026-09-26 09:26:18 +0800

    Update: add-two-numbers - submission-1

--- Data Structures & Algorithms/add-two-numbers/submission-1.java
+++ Data Structures & Algorithms/add-two-numbers/submission-1.java
@@ -11,24 +11,46 @@
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode(); // Dummy node to build the result list
         ListNode cur = dummy; 
 
-        int carry = 0;
+        int carry = 0; // Carry from the previous addition
 
-        while (l1 != null | l2 != null || carry != 0){
-            int v1 = (l1 != null) ? l1.val : 0;
+        // Continue while there are digits to process
+        // or we still have a carry
+        while (l1 != null || l2 != null || carry != 0){
+            int v1 = (l1 != null) ? l1.val : 0; 
             int v2 = (l2 != null) ? l2.val : 0;
 
             int sum = v1 + v2 + carry; 
-            carry = sum / 10; 
-            int digit = sum % 10;
-            cur.next = new ListNode(digit);
-            cur = cur.next; 
+            carry = sum / 10; // 15 / 10 = 1
+            int digit = sum % 10; // 15 % 10 = 5
+            cur.next = new ListNode(digit); // Add the sum to the result list
 
-            l1 = (l1 != null) ? l1.next : null;
+            cur = cur.next;
+            l1 = (l1 != null) ? l1.next : null; // Move to the next nodes
             l2 = (l2 != null) ? l2.next : null;
         }
-        return dummy.next; 
+        return dummy.next;
     }
 }
+
+
+//Time Complexity: O(m + n)
+//Space Complexity: 0(1) extra space, 0(max(m,n))
+//where m = length of l1 and n = length of l2
+
+//Because the digits are stored in reverse order, I can add the two lists from //head to tail just like normal addition. For each position, I add the two digits //and the carry. The current digit is sum % 10, and the carry is sum / 10. I use //a dummy node to build the result list
+
+// dummy node
+//     ↓
+// get digit 1
+// get digit 2
+// add carry
+//     ↓
+// digit = sum % 10
+// carry = sum / 10
+//     ↓
+// create node
+// move pointers
+
